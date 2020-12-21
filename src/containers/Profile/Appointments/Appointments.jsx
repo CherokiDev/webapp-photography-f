7e5d71362dc27bc7838a85fc93acceed09cbf12d
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-12-21 19:57:22 +0100

    added modal when creating and deleting an appointment

--- src/containers/Profile/Appointments/Appointments.jsx
+++ src/containers/Profile/Appointments/Appointments.jsx
@@ -2,9 +2,66 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { connect } from 'react-redux'
+import Swal from 'sweetalert2';
+import { useHistory } from 'react-router-dom';
 
 const Appointments = (props) => {
     const [availableDates, setAvailableDates] = useState([])
+    const [msg, setMsg] = useState("")
+    const [dataAppointment, setDataAppointment] = useState()
+    const checkToken = props.user.token
+    /* const withAppointment = "Ya tienes una cita" */
+    const withoutAppointment = "No tienes ninguna cita"
+    const history = useHistory();
+
+    const statusAppointment = {
+        status: "Disponible"
+    }
+
+    useEffect(() => {
+        const userId = async () => {
+            await axios.get('http://localhost:3005/appointments/byUserId/' + props.user.id, {
+                headers: {
+                    Authorization: "Bearer " + checkToken
+                }
+            })
+                .then((res) => {
+                    if (!res.data.UserId) {
+                        setMsg(withoutAppointment)
+                    } else {
+                        axios.get('http://localhost:3005/dateappointments/getById/' + res.data.DateappointmentId)
+                            .then((res) => {
+                                setMsg(`Tienes una cita para la fecha ${res.data.date}`)
+                            }).catch((err) => {
+                                return err
+                            });
+                        setDataAppointment(res.data)
+                        /* setMsg(withAppointment) */
+                    }
+                }).catch((err) => {
+                    return err
+                });
+        }
+        userId()
+        // eslint-disable-next-line
+    }, [])
+
+    const canceledAppointment = async () => {
+        await axios.put('http://localhost:3005/dateappointments/update/' + dataAppointment.DateappointmentId, statusAppointment)
+            .then(() => {
+                axios.delete('http://localhost:3005/appointments/delete/' + dataAppointment.id, {
+                    headers: {
+                        Authorization: "Bearer " + checkToken
+                    }
+                })
+                Swal.fire("Cita eliminada correctamente")
+                setTimeout(() => {
+                    history.push('/profile')
+                }, 1000)
+            }).catch((err) => {
+                return err
+            });
+    }
 
     const getAvailableDates = async () => {
         await axios.get('http://localhost:3005/dateappointments/availableDates')
@@ -44,6 +101,10 @@ const Appointments = (props) => {
         })
             .then((res) => {
                 axios.put('http://localhost:3005/dateappointments/update/' + appointmentsData.DateappointmentId, statusAppointment)
+                Swal.fire("Cita creada correctamente")
+                setTimeout(() => {
+                    history.push('/profile')
+                }, 1000)
                 return res
             }).catch((err) => {
                 return err
@@ -53,27 +114,39 @@ const Appointments = (props) => {
     return (
         <>
             <div>Citas</div>
-            <form action="" onSubmit={handleSubmit}>
-                <p>Formulario de citas</p>
-                <p>Tipo</p>
-                <select name="type" required>
-                    <option value="">Tipo de cita</option>
-                    <option value="Recién nacido">Recién nacido</option>
-                    <option value="Comunión">Comunión</option>
-                    <option value="Seguimiento">Seguimiento</option>
-                </select>
-                <p>Observaciones</p>
-                <textarea name="observations" id="" cols="30" rows="10"></textarea>
-                <p>Citas disponibles</p>
+            <div>{msg}</div>
+            {msg === withoutAppointment
+                ?
+                <form action="" onSubmit={handleSubmit}>
+                    <p>Formulario de citas</p>
+                    <p>Tipo</p>
+                    <select name="type" required>
+                        <option value="">Tipo de cita</option>
+                        <option value="Recién nacido">Recién nacido</option>
+                        <option value="Comunión">Comunión</option>
+                        <option value="Seguimiento">Seguimiento</option>
+                    </select>
+                    <p>Observaciones</p>
+                    <textarea name="observations" cols="30" rows="10"></textarea>
+                    <p>Citas disponibles</p>
 
-                <select name="DateappointmentId" id="" size="5" required>
-                    {availableDates?.map(date =>
-                        <option key={date.id} value={date.id}>{date.date}</option>
-                    )}
-                </select>
-                <div></div>
-                <button type="submit">Enviar</button>
-            </form>
+                    <select name="DateappointmentId" size="5" required>
+                        {availableDates?.map(date =>
+                            <option key={date.id} value={date.id}>{date.date}</option>
+                        )}
+                    </select>
+                    <div></div>
+                    <button type="submit">Crear cita</button>
+                </form>
+                :
+                <>
+                    <div>Solo puedes tener una cita</div>
+                    <button onClick={canceledAppointment}>Eliminar cita actual</button>
+                </>
+            }
+            <div></div>
+
+
             <button><Link to="/profile">Volver</Link></button>
         </>
     )
--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
@@ -7,72 +7,18 @@ import { LOGOUT } from '../../redux/types/userType';
 const Profile = (props) => {
 
     const history = useHistory();
-    const [msg, setMsg] = useState("")
-    const [dataAppointment, setDataAppointment] = useState()
-    const checkToken = props.user.token
-    const withAppointment = "Ya tienes una cita"
-    const withoutAppointment = "No tienes ninguna cita"
-
-    const statusAppointment = {
-        status: "Disponible"
-    }
-
-    useEffect(() => {
-        const userId = async () => {
-            await axios.get('http://localhost:3005/appointments/byUserId/' + props.user.id, {
-                headers: {
-                    Authorization: "Bearer " + checkToken
-                }
-            })
-                .then((res) => {
-                    if (!res.data.UserId) {
-                        setMsg(withoutAppointment)
-                    } else {
-                        setDataAppointment(res.data)
-                        setMsg(withAppointment)
-                    }
-                }).catch((err) => {
-                    return err
-                });
-        }
-        userId()
-        // eslint-disable-next-line
-    }, [])
-
-    const canceledAppointment = async () => {
-        await axios.put('http://localhost:3005/dateappointments/update/' + dataAppointment.DateappointmentId, statusAppointment)
-            .then(() => {
-                axios.delete('http://localhost:3005/appointments/delete/' + dataAppointment.id, {
-                    headers: {
-                        Authorization: "Bearer " + checkToken
-                    }
-                })
-            }).catch((err) => {
-                return err
-            });
-    }
-
+  
     const logout = async () => {
         await axios.put('http://localhost:3005/users/logout/' + props.user.email)
         props.dispatch({ type: LOGOUT, payload: {} });
-        setTimeout(() => {
-            history.push('/')
-        }, 1000)
+        history.push('/')
+        
     }
 
     return (
         <>
             <div>Perfil</div>
-            <div>{msg}</div>
-            {msg === withoutAppointment
-                ?
-                <button><Link to="/profile/appointments">Pedir una cita</Link> </button>
-                :
-                <>
-                    <div>Solo puedes tener una cita</div>
-                    <button onClick={canceledAppointment}>Cancelar / modificar cita actual</button>
-                </>
-            }
+            <button><Link to="/profile/appointments">Gestionar mi cita</Link></button>
             <div></div>
             <button onClick={logout}>Salir</button>
         </>
