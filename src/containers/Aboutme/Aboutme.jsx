0461c23f63efabe6e71a2f3cc92bd26c9234ee44
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-01-08 19:08:18 +0100

    some changes in the text of the Aboutme.jsx

--- src/containers/Aboutme/Aboutme.jsx
+++ src/containers/Aboutme/Aboutme.jsx
@@ -14,9 +14,10 @@ const Aboutme = () => {
                     </div>
                     <div className="aboutMeText">
                         <p>
-                            Detrás del proyecto El Barquito de Papel me encuentro yo, Isabel Mª Barrera, una mujer sencilla, paciente y trabajadora pero ante todo mamá de dos hijos. Ellos son el motor de mi vida y uno de los motivos por el cual hoy en día me dedico a la fotografía infantil. </p>
+                            Detrás del proyecto El Barquito de Papel me encuentro yo, Isabel Mª Barrera, una mujer sencilla, paciente y trabajadora. Pero, ante todo, mamá de dos hijos. Ellos son el motor de mi vida y uno de los motivos por los cuales hoy en día me dedico a la fotografía infantil.
+                        </p>
                         <p>
-                            Ser madre me facilita el trato con mis "miniclientes" porque cuando tengo que fotografiarles y a veces no quieren, se asustan o simplemente empiezan a llorar, intento calmarles, jugar con ellos y que pasen un rato divertido y se olviden que están en un estudio ¡Y casi siempre funciona!
+                            Ser madre me facilita el trato con mis "miniclientes", ya que cuando tengo que fotografiarlos, hay veces en las que no quieren, se asustan o simplemente empiezan a llorar. Entonces, intento calrmarlos, jugar con ellos y que pasen un rato divertido, y se olviden de que están en un estudio. ¡Y casi siempre funciona!
                         </p>
 
                     </div>
@@ -25,10 +26,10 @@ const Aboutme = () => {
                 <div className="aboutMeRows">
                     <div className="aboutMeText">
                         <p>
-                            Esta pasión por la fotografía empezó, por supuesto, haciéndole fotos a mis hijos y los niños de mi entorno. Hasta que en el año 2016 decidí darme un caprichito y comprarme mi primera camara réflex para seguir con un hobbie que cada día me gustaba más y sobre el cual quería saber  cada vez más. Poco a poco y con pasos firmes he llegado hasta lo que soy hoy y aunque sé que todavía me queda muchísimo camino que recorrer espero hacerlo igual que hasta ahora, con calma, paciencia y vuestro cariño y apoyo.
+                            Esta pasión por la fotografía empezó, por supuesto, haciendo fotos a mis hijos y a los niños de mi entorno. Hasta que, en el año 2016, decidí darme un caprichito y comprarme mi primera cámara réflex para seguir con un hobby que cada día me gustaba más, y sobre el cual quería saber  cada vez más. Poco a poco, y con pasos firmes, he llegado hasta lo que soy hoy y, aunque sé que todavía me queda muchísimo camino por recorrer, espero hacerlo igual que hasta ahora: con calma, paciencia y con vuestro cariño y apoyo.
                         </p>
                         <p>
-                            Nos vemos en el estudio.
+                            ¡Nos vemos en el estudio!
                         </p>
                     </div>
                     <div className="aboutMePhoto">
