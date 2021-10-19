0f1a202b0db8f70defd5f8705d0b23315203f49d
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-10-19 18:54:30 +0200

    changed connect by useSelector in ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-const ProtectedRoute = (props) => {
-    return props.user?.uid ? props.children : <Redirect to='/login' />
+const ProtectedRoute = ({ children }) => {
+    const { uid } = useSelector(state => state.user)
+    return uid ? children : <Redirect to='/login' />
 }
 
-const mapStateToProps = state => {
-    return {
-        user: state.user
-    }
-}
-
-export default connect(mapStateToProps)(ProtectedRoute);
+export default ProtectedRoute;
